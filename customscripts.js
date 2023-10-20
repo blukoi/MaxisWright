dcfd947c2cd24d392aeabc0fbaaf9ac768612944
Max Wright <47584716+blukoi@users.noreply.github.com>
2023-10-19 17:39:37 -0700

    Linked JS together

--- customscripts.js
+++ customscripts.js
@@ -32,3 +32,108 @@ document.addEventListener('DOMContentLoaded', function () {
         document.removeEventListener('click', closeOnClickOutside);
     }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const ffReadMore = document.getElementById('ff-designing-readmore');
+    const ffBackground = document.getElementById('ff-designing-bg');
+    const ffModal = document.getElementById('ff-designing-modal');
+    const ffClose = document.getElementById('ff-designing-close');
+
+    ffReadMore.addEventListener('click', function () {
+        ffBackground.classList.add('activebg');
+        ffModal.classList.add('activemodal');
+        addClickOutsideListener();
+    });
+
+    ffClose.addEventListener('click', function () {
+        ffBackground.classList.remove('activebg');
+        ffModal.classList.remove('activemodal');
+        removeClickOutsideListener();
+    });
+
+    function closeOnClickOutside(event) {
+        if (event.target === ffBackground) {
+            ffBackground.classList.remove('activebg');
+            ffModal.classList.remove('activemodal');
+            removeClickOutsideListener();
+        }
+    }
+
+    function addClickOutsideListener() {
+        document.addEventListener('click', closeOnClickOutside);
+    }
+
+    function removeClickOutsideListener() {
+        document.removeEventListener('click', closeOnClickOutside);
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const ffReadMore = document.getElementById('ff-revisions-readmore');
+    const ffBackground = document.getElementById('ff-revisions-bg');
+    const ffModal = document.getElementById('ff-revisions-modal');
+    const ffClose = document.getElementById('ff-revisions-close');
+
+    ffReadMore.addEventListener('click', function () {
+        ffBackground.classList.add('activebg');
+        ffModal.classList.add('activemodal');
+        addClickOutsideListener();
+    });
+
+    ffClose.addEventListener('click', function () {
+        ffBackground.classList.remove('activebg');
+        ffModal.classList.remove('activemodal');
+        removeClickOutsideListener();
+    });
+
+    function closeOnClickOutside(event) {
+        if (event.target === ffBackground) {
+            ffBackground.classList.remove('activebg');
+            ffModal.classList.remove('activemodal');
+            removeClickOutsideListener();
+        }
+    }
+
+    function addClickOutsideListener() {
+        document.addEventListener('click', closeOnClickOutside);
+    }
+
+    function removeClickOutsideListener() {
+        document.removeEventListener('click', closeOnClickOutside);
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const ffReadMore = document.getElementById('ff-finalthoughts-readmore');
+    const ffBackground = document.getElementById('ff-finalthoughts-bg');
+    const ffModal = document.getElementById('ff-finalthoughts-modal');
+    const ffClose = document.getElementById('ff-finalthoughts-close');
+
+    ffReadMore.addEventListener('click', function () {
+        ffBackground.classList.add('activebg');
+        ffModal.classList.add('activemodal');
+        addClickOutsideListener();
+    });
+
+    ffClose.addEventListener('click', function () {
+        ffBackground.classList.remove('activebg');
+        ffModal.classList.remove('activemodal');
+        removeClickOutsideListener();
+    });
+
+    function closeOnClickOutside(event) {
+        if (event.target === ffBackground) {
+            ffBackground.classList.remove('activebg');
+            ffModal.classList.remove('activemodal');
+            removeClickOutsideListener();
+        }
+    }
+
+    function addClickOutsideListener() {
+        document.addEventListener('click', closeOnClickOutside);
+    }
+
+    function removeClickOutsideListener() {
+        document.removeEventListener('click', closeOnClickOutside);
+    }
+});
