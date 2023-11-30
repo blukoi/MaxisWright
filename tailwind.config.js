d0a39c6233b82bfd2f24181512e9df912c7bea6b
Max Wright <max.wri@gmail.com>
2023-11-29 21:45:46 -0800

    NPM Installation of Tailwind and rewrote About page

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+          mint: '#00ab8f',
+          lightishblue: '#eff7ff',
+          fontfacingblue: '#122f3c',
+          tattooblue: '#4285f4',
+          tattooalert: '#ff6f6f',
+          academyblue: '#2e95e0',
+          academygold: '#b89848',
+          neutralbg: '#d9d9d9',
+      }
+    },
+  },
+  plugins: [],
+}
+
